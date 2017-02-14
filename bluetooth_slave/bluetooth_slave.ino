7c0028e548d51d799e099bd4940763562c926a65
weshays <weshays@gbdev.com>
2017-02-14 14:24:30 -0800

    Checkin point for slave

--- bluetooth_slave/bluetooth_slave.ino
+++ bluetooth_slave/bluetooth_slave.ino
@@ -1,24 +1,33 @@
-int state = ' ';
-int ledPin = 13;
+char data[] = "+000|090";  // Array of size 9 (8 characters + null terminate).
+                           // Direction and Speed | Turn.  +/- for direction.
+                           // Speed is from -100 to 100, and -5 to 5 will be 0.
+                           // Turn 0 to 180, and 85 to 95 will be 90. 
+
+int turn = 90;             // Start out with no turn.
+int vector = 0;            // Start out not moving.
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
   Serial.begin(38400); // Default communication rate of the Bluetooth module
 }
 
 void loop() {
-  if(Serial.available() > 0){ // Checks whether data is comming from the serial port
-    state = Serial.read(); // Reads the data from the serial port
-    Serial.println(state);
-    if (state == 97) { // 'a'
-      Serial.println("LED ON");
-      digitalWrite(ledPin, HIGH); // LED ON
-    } else if (state == 98) { // 'b'
-      Serial.println("LED OFF");
-      digitalWrite(ledPin, LOW); // LED OFF
-    }
+  if(Serial.available() > 0) { // Checks whether data is comming from the serial port.
+    parseCommands();           // Parse data from Master.
+    setTurn();
+    setSpeedAndDirection();
   }
 }
+
+void parseCommands() {
+  String new_data = Serial.readStringUntil('\n'); // Array of size 9 (8 characters + null terminate).
+  new_data.toCharArray(data, 9);
+}
+
+void setTurn() {
+  
+}
+
+void setSpeedAndDirection() {
+  
+}
+
