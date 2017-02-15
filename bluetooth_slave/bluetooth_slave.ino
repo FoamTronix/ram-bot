68b0725acc147271de731ca9dc5433db76e89cef
weshays <weshays@gbdev.com>
2017-02-15 08:15:42 -0800

    Started refactor.  WIP.

--- bluetooth_slave/bluetooth_slave.ino
+++ bluetooth_slave/bluetooth_slave.ino
@@ -14,49 +14,50 @@ int vector = 0;            // Start out not moving.
 
 void setup() {
   Serial.begin(9600);
-  BTSerial.begin(38400);           // Transmission speed used on the bluetooth module  
+  BTSerial.begin(38400);   // Transmission speed used on the bluetooth module  
 }
 
 void loop() {
   if(receivedCommand()) {
     setTurn();
     setSpeedAndDirection();
+    engage();
   }
 }
 
 boolean receivedCommand() {
   if(BTSerial.available() && BTSerial.read() == '^') {  // Check if transmission started
     for(int i = 0; i < 8; i++) { 
-      while(!BTSerial.available()); // Wait for data to show up
+      while(!BTSerial.available());                     // Wait for data to show up.
       data[i] = BTSerial.read(); 
     }
   } else {
     return false;                                       // Transmission has not started yet.
   }
-  return validateCommand(); 
+  return validateCommand();                             // Validate the command that has been received.
 }
 
 boolean validateCommand() {
   int i;
-  if(data[0] != '+' && data[0] != '-') { return false; }
-  if(data[4] != '|') { return false; }
-  for(i=1; i<=3; i++) { if(!isDigit(data[i])) { return false; } }
-  for(i=5; i<=7; i++) { if(!isDigit(data[i])) { return false; } }
+  if(data[0] != '+' && data[0] != '-') { return false; }           // [0] must be a + or -
+  for(i=1; i<=3; i++) { if(!isDigit(data[i])) { return false; } }  // [1,2,3] must be an integer
+  if(data[4] != '|') { return false; }                             // [4] must be a |
+  for(i=5; i<=7; i++) { if(!isDigit(data[i])) { return false; } }  // [5,6,7] must be an integer
   return true;  
 }
 
 void setTurn() {
   char new_turn[] = { data[5], data[6], data[7], '\0' };      
   turn = atoi(new_turn);
-  Serial.print("Turn: ");
-  Serial.print(turn);
 }
 
 void setSpeedAndDirection() {
   char new_speed[] = { data[1], data[2], data[3], '\0' }; 
   vector = atoi(new_speed);
-  if(data[0] == '-') { vector *= -1; }
-  Serial.print(" | Vector: ");
-  Serial.println(vector);     
+  if(data[0] == '-') { vector *= -1; }  
+}
+
+void engage() {
+
 }
 
