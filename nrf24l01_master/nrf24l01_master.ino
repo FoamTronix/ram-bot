6ebed97718d6a690a0ab955ea69bc086a6c6edb3
weshays <weshays@gbdev.com>
2017-04-02 14:23:53 -0700

    Working on nrf24l01 master

--- nrf24l01_master/nrf24l01_master.ino
+++ nrf24l01_master/nrf24l01_master.ino
@@ -1,64 +1,142 @@
-#include <SoftwareSerial.h>
-                                  
-                                   //  The circuit:
-                                   // * RX is digital pin 10 (connect to TX of other device)
-                                   // * TX is digital pin 11 (connect to RX of other device)
-#define TxD 2                      // Digital pin used to transmit data to the bluetooth module
-#define RxD 3                      // Digital pin used to receive data from the bluetooth module
-SoftwareSerial BTSerial(RxD, TxD); // RX, TX for Bluetooth
 
-#define JoystickUD 3               // Analog pin used for Up/Down on the Parallax Thumbstick
-#define JoystickLR 4               // Analog pin used for Left/Right on the Parallax Thumbstick
+/*
+* Getting Started example sketch for nRF24L01+ radios
+* This is a very basic example of how to send data from one node to another
+* Updated: Dec 2014 by TMRh20
+*/
 
-char data[] = "^+000|000";          // Array of size 9 (8 characters + null terminate).
-                                   // Direction and Speed | Turn.  +/- for direction.
-                                   // Speed is from -100 to 100, and -5 to 5 will be 0.
-                                   // Turn 0 to 180, and 85 to 95 will be 90. 
+#include <SPI.h>
+#include "RF24.h"
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 0;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+/**********************************************************/
+
+byte addresses[][6] = {"1Node","2Node"};
+
+// Used to control whether this node is sending or receiving
+bool role = 0;
 
 void setup() {
-  Serial.begin(9600);
-  BTSerial.begin(38400);           // Transmission speed used on the bluetooth module
+  Serial.begin(115200);
+  Serial.println(F("RF24/examples/GettingStarted"));
+  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+  
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+ // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
+  
+  // Start the radio listening for data
+  radio.startListening();
 }
 
 void loop() {
-  setDirectionAndSpeed();
-  setTurn();
-  sendData();
-}
+  
+  
+/****************** Ping Out Role ***************************/  
+if (role == 1)  {
+    
+    radio.stopListening();                                    // First, stop listening so we can talk.
+    
+    
+    Serial.println(F("Now sending"));
 
-void setDirectionAndSpeed() {
-  char vector[4];  // 3 characters + '\0'
-  int vectorValRaw = map(analogRead(JoystickUD), 0, 1023, -100, 100);
-  if(vectorValRaw >= -5 && vectorValRaw <= 5) { vectorValRaw = 0; }
-  String vectorVal = String( abs(vectorValRaw) );
-  vectorVal.toCharArray(vector, 4); 
-  replaceRange(2, 4, vector);
-  data[1] = vectorValRaw >= 0 ? '+' : '-';
-}
+    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
+     if (!radio.write( &start_time, sizeof(unsigned long) )){
+       Serial.println(F("failed"));
+     }
+        
+    radio.startListening();                                    // Now, continue listening
+    
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout ){                                             // Describe the results
+        Serial.println(F("Failed, response timed out."));
+    }else{
+        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
+        radio.read( &got_time, sizeof(unsigned long) );
+        unsigned long end_time = micros();
+        
+        // Spew it
+        Serial.print(F("Sent "));
+        Serial.print(start_time);
+        Serial.print(F(", Got response "));
+        Serial.print(got_time);
+        Serial.print(F(", Round-trip delay "));
+        Serial.print(end_time-start_time);
+        Serial.println(F(" microseconds"));
+    }
 
-void setTurn() {
-  char turn[4]; // 3 characters + '\0'
-  int turnValRaw = map(analogRead(JoystickLR), 0, 1023, 0, 180);
-  if(turnValRaw >= 85 && turnValRaw <= 95) { turnValRaw = 90; }
-  String turnVal = String(turnValRaw);
-  turnVal.toCharArray(turn, 4);
-  replaceRange(6, 8, turn);
-}
+    // Try again 1s later
+    delay(1000);
+  }
 
-void replaceRange(int start_pos, int end_pos, char* vals) {
-  int next_pos = strlen(vals) - 1;             // -1 so it can be used directly as an index
-  for(int i = end_pos; i >= start_pos; i--) {  
-    if(next_pos >= 0) {
-      data[i] = vals[next_pos];
-      next_pos--;   
-    } else {
-      data[i] = '0';                           // Zero out the data
+
+
+/****************** Pong Back Role ***************************/
+
+  if ( role == 0 )
+  {
+    unsigned long got_time;
+    
+    if( radio.available()){
+                                                                    // Variable for the received timestamp
+      while (radio.available()) {                                   // While there is data ready
+        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
+      }
+     
+      radio.stopListening();                                        // First, stop listening so we can talk   
+      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
+      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
+      Serial.print(F("Sent response "));
+      Serial.println(got_time);  
+   }
+ }
+
+
+
+
+/****************** Change Roles via Serial Commands ***************************/
+
+  if ( Serial.available() )
+  {
+    char c = toupper(Serial.read());
+    if ( c == 'T' && role == 0 ){      
+      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
+      role = 1;                  // Become the primary transmitter (ping out)
+    
+   }else
+    if ( c == 'R' && role == 1 ){
+      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
+       role = 0;                // Become the primary receiver (pong back)
+       radio.startListening();
+       
     }
   }
-}
 
-void sendData() {
-  BTSerial.write(data);
-  Serial.println(data);
-}
+
+} // Loop
 
